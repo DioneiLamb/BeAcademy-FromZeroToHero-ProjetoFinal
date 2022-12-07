ea0f414cccd840d8cddf9abd776cc19b2e6f4418
Dionei Rogério Lamb <dionei.lamb@yahoo.com.br>
2022-12-06 23:02:06 -0300

    atualização de casa
    não deu muito certo hoje continuar de casa as melhorias necessárias

--- Landpage/js/scripts_olho.js
+++ Landpage/js/scripts_olho.js
@@ -1,41 +1,131 @@
-// mostra e econde no olho login
-let btn = document.querySelector('.fa-eye') //adicionei a classe .fa-eye na minha variável btn
+let btn = document.querySelector('#verSenha')
+let btnConfirm = document.querySelector('#verConfirmSenha')
 
-btn.addEventListener('click', () =>{ //esse meu btn recebe um evento click que ao ser clicado vai executar uma função
-    let input_senha = document.querySelector ('#senha') //essa função vai criar uma variável input_senha e vai pegar o id senha do input 
 
-    if(input_senha.getAttribute('type') == 'password') { //se o meu input_senha tiver o atributo type e se esse atributo for do tipo password eu pego ele e...
-        input_senha.setAttribute('type','text') //troco pelo tipo text
-        } else { //senão
-            input_senha.setAttribute('type','password') //faça ao contrário
-        }
+let nome = document.querySelector('#nome')
+let labelNome = document.querySelector('#labelNome')
+let validNome = false
+
+let usuario = document.querySelector('#usuario')
+let labelUsuario = document.querySelector('#labelUsuario')
+let validUsuario = false
+
+let senha = document.querySelector('#senha')
+let labelSenha = document.querySelector('#labelSenha')
+let validSenha = false
+
+let confirmSenha = document.querySelector('#confirmSenha')
+let labelConfirmSenha = document.querySelector('#labelConfirmSenha')
+let validConfirmSenha = false
+
+let msgError = document.querySelector('#msgError')
+let msgSuccess = document.querySelector('#msgSuccess')
+
+nome.addEventListener('keyup', () => {
+  if(nome.value.length <= 2){
+    labelNome.setAttribute('style', 'color: red')
+    labelNome.innerHTML = 'Nome *Insira no minimo 3 caracteres'
+    nome.setAttribute('style', 'border-color: red')
+    validNome = false
+  } else {
+    labelNome.setAttribute('style', 'color: green')
+    labelNome.innerHTML = 'Nome'
+    nome.setAttribute('style', 'border-color: green')
+    validNome = true
+  }
 })
 
-//##########################
-
-// mostra e econde no olho no campo senha do cadastro 
-let btn1 = document.querySelector('#ver_senha') //adicionei a classe .fa-eye na minha variável btn
-
-btn1.addEventListener('click', () =>{ //esse meu btn recebe um evento click que ao ser clicado vai executar uma função
-    let input_senha = document.querySelector ('#label_v_senha') //essa função vai criar uma variável input_senha e vai pegar o id senha do input 
-
-    if(input_senha.getAttribute('type') == 'password') { //se o meu input_senha tiver o atributo type e se esse atributo for do tipo password eu pego ele e...
-        input_senha.setAttribute('type','text') //troco pelo tipo text
-        } else { //senão
-            input_senha.setAttribute('type','password') //faça ao contrário
-        }
+usuario.addEventListener('keyup', () => {
+  if(usuario.value.length <= 4){
+    labelUsuario.setAttribute('style', 'color: red')
+    labelUsuario.innerHTML = 'Usuário *Insira no minimo 5 caracteres'
+    usuario.setAttribute('style', 'border-color: red')
+    validUsuario = false
+  } else {
+    labelUsuario.setAttribute('style', 'color: green')
+    labelUsuario.innerHTML = 'Usuário'
+    usuario.setAttribute('style', 'border-color: green')
+    validUsuario = true
+  }
 })
 
-//################
-// mostra e econde no olho no campo senha do cadastro 
-let btn2 = document.querySelector('#ver_confirm_senha') //adicionei a classe .fa-eye na minha variável btn
+senha.addEventListener('keyup', () => {
+  if(senha.value.length <= 5){
+    labelSenha.setAttribute('style', 'color: red')
+    labelSenha.innerHTML = 'Senha *Insira no minimo 6 caracteres'
+    senha.setAttribute('style', 'border-color: red')
+    validSenha = false
+  } else {
+    labelSenha.setAttribute('style', 'color: green')
+    labelSenha.innerHTML = 'Senha'
+    senha.setAttribute('style', 'border-color: green')
+    validSenha = true
+  }
+})
 
-btn2.addEventListener('click', () =>{ //esse meu btn recebe um evento click que ao ser clicado vai executar uma função
-    let input_senha = document.querySelector ('#label_v_conf_senha') //essa função vai criar uma variável input_senha e vai pegar o id senha do input 
+confirmSenha.addEventListener('keyup', () => {
+  if(senha.value != confirmSenha.value){
+    labelConfirmSenha.setAttribute('style', 'color: red')
+    labelConfirmSenha.innerHTML = 'Confirmar Senha *As senhas não conferem'
+    confirmSenha.setAttribute('style', 'border-color: red')
+    validConfirmSenha = false
+  } else {
+    labelConfirmSenha.setAttribute('style', 'color: green')
+    labelConfirmSenha.innerHTML = 'Confirmar Senha'
+    confirmSenha.setAttribute('style', 'border-color: green')
+    validConfirmSenha = true
+  }
+})
 
-    if(input_senha.getAttribute('type') == 'password') { //se o meu input_senha tiver o atributo type e se esse atributo for do tipo password eu pego ele e...
-        input_senha.setAttribute('type','text') //troco pelo tipo text
-        } else { //senão
-            input_senha.setAttribute('type','password') //faça ao contrário
-        }
+function cadastrar(){
+  if(validNome && validUsuario && validSenha && validConfirmSenha){
+    let listaUser = JSON.parse(localStorage.getItem('listaUser') || '[]')
+    
+    listaUser.push(
+    {
+      nomeCad: nome.value,
+      userCad: usuario.value,
+      senhaCad: senha.value
+    }
+    )
+    
+    localStorage.setItem('listaUser', JSON.stringify(listaUser))
+    
+   
+    msgSuccess.setAttribute('style', 'display: block')
+    msgSuccess.innerHTML = '<strong>Cadastrando usuário...</strong>'
+    msgError.setAttribute('style', 'display: none')
+    msgError.innerHTML = ''
+    
+    setTimeout(()=>{
+        window.location.href = 'https://cdpn.io/thicode/debug/ZELzYxV/dXAqBaRyvwJk'
+    }, 3000)
+  
+    
+  } else {
+    msgError.setAttribute('style', 'display: block')
+    msgError.innerHTML = '<strong>Preencha todos os campos corretamente antes de cadastrar</strong>'
+    msgSuccess.innerHTML = ''
+    msgSuccess.setAttribute('style', 'display: none')
+  }
+}
+
+btn.addEventListener('click', ()=>{
+  let inputSenha = document.querySelector('#senha')
+  
+  if(inputSenha.getAttribute('type') == 'password'){
+    inputSenha.setAttribute('type', 'text')
+  } else {
+    inputSenha.setAttribute('type', 'password')
+  }
+})
+
+btnConfirm.addEventListener('click', ()=>{
+  let inputConfirmSenha = document.querySelector('#confirmSenha')
+  
+  if(inputConfirmSenha.getAttribute('type') == 'password'){
+    inputConfirmSenha.setAttribute('type', 'text')
+  } else {
+    inputConfirmSenha.setAttribute('type', 'password')
+  }
 })
